645cdad328918ea3971acd3931cf64276f3f4c5d
Aron Tsang <arontsang@gmail.com>
2021-03-31 01:15:21 +0800

    Lifetimes

--- src/listener/udp.rs
+++ src/listener/udp.rs
@@ -1,80 +1,177 @@
 use crate::resolver::DnsResolver;
+use async_executor::LocalExecutor;
+use async_task::Task;
 use arr_macro::arr;
 use bytes::{BytesMut, Bytes, BufMut};
-use tokio::task;
+use futures_lite::future;
 use tokio::net::UdpSocket;
 use std::net::{SocketAddr};
 use std::error::Error;
 use std::sync::Arc;
-pub async fn start<T : DnsResolver + 'static >(bind_address: SocketAddr, resolver: std::rc::Rc<T>) -> Result<(), Box<dyn Error>> {
-    let socket = Arc::new(UdpSocket::bind(bind_address).await?);
-    let (receiver, sender) = (socket.clone(), socket);
-    let (reply_queue, reply_tasks) = tokio::sync::mpsc::channel::<(SocketAddr, Bytes)>(100);
 
-    let listen_request_task = task::spawn_local(async move {
-        let mut queue = reply_tasks;
-        let sender = sender;
-        loop {
-            let success = match queue.recv().await {
-                Some((client, payload)) =>
-                    Some(sender.send_to(&payload, &client).await),
-                None => None,
-            };
-            match success {
-                Some(Ok(_)) => {},
-                Some(Err(error)) =>
-                    println!("Error sending reply: {}", error),
-                None => {},
-            }
-        }
-    });
+pub struct UdpServer<T : DnsResolver + 'static > {
 
-    let (received_packet_enqueue, mut dequeue_received_packet) = tokio::sync::mpsc::channel::<(SocketAddr, Bytes)>(100);
+    resolver: std::marker::PhantomData<T>,
+    task: Task<()>
+}
 
-    let receiving_task = task::spawn(async move {
-        loop {
-            let mut read_buffer = arr![0u8; 1500];
+impl<T : DnsResolver + 'static> Drop for UdpServer<T>{
+    fn drop(&mut self) {
+        drop(&mut self.task);
+    }
+}
 
-            match receiver.recv_from(&mut read_buffer).await {
-                Ok((length, client)) => {
-                    let mut buffer = BytesMut::with_capacity(1500);
-                    buffer.put(&read_buffer[0..length]);
+impl<T : DnsResolver + 'static> UdpServer<T> {
+    pub async fn new(executor: &LocalExecutor<'_>, bind_address: SocketAddr, resolver: std::rc::Rc<T>) -> Result<UdpServer<T>, Box<dyn Error>> {
 
-                    let query = buffer.freeze();
-                    received_packet_enqueue.send((client, query)).await.unwrap();
-                },
-                Err(error) => {
-                    println!("Error receiving packet: {}", error)
+        let server_executor = LocalExecutor::new();
+
+        let socket = UdpSocket::bind(bind_address).await?;
+        let socket = Arc::new(socket);
+
+        let (receiver, sender) = (socket.clone(), socket);
+        let (reply_queue, reply_tasks) = tokio::sync::mpsc::channel::<(SocketAddr, Bytes)>(100);
+
+        server_executor.spawn(async move {
+            let mut queue = reply_tasks;
+            let sender = sender;
+            loop {
+                let success = match queue.recv().await {
+                    Some((client, payload)) =>
+                        Some(sender.send_to(&payload, &client).await),
+                    None => None,
+                };
+                match success {
+                    Some(Ok(_)) => {},
+                    Some(Err(error)) =>
+                        println!("Error sending reply: {}", error),
+                    None => {},
                 }
             }
-        }
-    });
+        }).detach();
 
-    let request_dispatch_task = task::spawn_local(async move {
+        let (received_packet_enqueue, mut dequeue_received_packet) = tokio::sync::mpsc::channel::<(SocketAddr, Bytes)>(100);
 
+        server_executor.spawn(async move {
+            loop {
+                let mut read_buffer = arr![0u8; 1500];
+                match receiver.recv_from(&mut read_buffer).await {
+                    Ok((length, client)) => {
+                        let mut buffer = BytesMut::with_capacity(1500);
+                        buffer.put(&read_buffer[0..length]);
 
-        while let Some((client, query)) = dequeue_received_packet.recv().await{
-            let local_resolver_copy = resolver.clone();
-            let local_reply_queue = reply_queue.clone();
-            task::spawn_local(async move {
-                match local_resolver_copy.resolve(query).await{
-                    Err(error) => println!("Error resolving packet: {}", error),
-                    Ok(reply) =>
-                        match local_reply_queue.send((client, reply)).await{
-                            Err(error) => println!("Error replying packet: {}", error),
-                            Ok(_) => {},
-                        }
-                };
-            });
-        }
+                        let query = buffer.freeze();
+                        received_packet_enqueue.send((client, query)).await.unwrap();
+                    },
+                    Err(error) => {
+                        println!("Error receiving packet: {}", error)
+                    }
+                }
+            }
+        }).detach();
 
+        server_executor.spawn(async move {
+            let continuation_executor = LocalExecutor::new();
+            let receiver_task = async {
+                while let Some((client, query)) = dequeue_received_packet.recv().await{
+                    let local_resolver_copy = resolver.clone();
+                    let local_reply_queue = reply_queue.clone();
+                    continuation_executor.spawn(async move {
+                        match local_resolver_copy.resolve(query).await{
+                            Err(error) => println!("Error resolving packet: {}", error),
+                            Ok(reply) =>
+                                match local_reply_queue.send((client, reply)).await{
+                                    Err(error) => println!("Error replying packet: {}", error),
+                                    Ok(_) => {},
+                                }
+                        };
+                    })
+                    .detach();
+                }
+            };
+            
+            future::or(continuation_executor.tick(), receiver_task).await
+        }).detach();
 
-    });
-
-    listen_request_task.await?;
-    request_dispatch_task.await?;
-    receiving_task.await?;
-
-    Ok(())
+        let task = executor.spawn(async move {
+            server_executor.tick().await
+        });
+        Ok(UdpServer::<T> { 
+            resolver: std::marker::PhantomData,
+            task
+        })
+    }
 }
+
+// pub async fn start<T : DnsResolver + 'static >(bind_address: SocketAddr, resolver: std::rc::Rc<T>) -> Result<(), Box<dyn Error>> {
+//     let socket = Arc::new(UdpSocket::bind(bind_address).await?);
+//     let (receiver, sender) = (socket.clone(), socket);
+//     let (reply_queue, reply_tasks) = tokio::sync::mpsc::channel::<(SocketAddr, Bytes)>(100);
+
+//     let listen_request_task = task::spawn_local(async move {
+//         let mut queue = reply_tasks;
+//         let sender = sender;
+//         loop {
+//             let success = match queue.recv().await {
+//                 Some((client, payload)) =>
+//                     Some(sender.send_to(&payload, &client).await),
+//                 None => None,
+//             };
+//             match success {
+//                 Some(Ok(_)) => {},
+//                 Some(Err(error)) =>
+//                     println!("Error sending reply: {}", error),
+//                 None => {},
+//             }
+//         }
+//     });
+
+//     let (received_packet_enqueue, mut dequeue_received_packet) = tokio::sync::mpsc::channel::<(SocketAddr, Bytes)>(100);
+
+//     let receiving_task = task::spawn(async move {
+//         loop {
+//             let mut read_buffer = arr![0u8; 1500];
+
+//             match receiver.recv_from(&mut read_buffer).await {
+//                 Ok((length, client)) => {
+//                     let mut buffer = BytesMut::with_capacity(1500);
+//                     buffer.put(&read_buffer[0..length]);
+
+//                     let query = buffer.freeze();
+//                     received_packet_enqueue.send((client, query)).await.unwrap();
+//                 },
+//                 Err(error) => {
+//                     println!("Error receiving packet: {}", error)
+//                 }
+//             }
+//         }
+//     });
+
+//     let request_dispatch_task = task::spawn_local(async move {
+
+
+//         while let Some((client, query)) = dequeue_received_packet.recv().await{
+//             let local_resolver_copy = resolver.clone();
+//             let local_reply_queue = reply_queue.clone();
+//             task::spawn_local(async move {
+//                 match local_resolver_copy.resolve(query).await{
+//                     Err(error) => println!("Error resolving packet: {}", error),
+//                     Ok(reply) =>
+//                         match local_reply_queue.send((client, reply)).await{
+//                             Err(error) => println!("Error replying packet: {}", error),
+//                             Ok(_) => {},
+//                         }
+//                 };
+//             });
+//         }
+
+
+//     });
+
+//     listen_request_task.await?;
+//     request_dispatch_task.await?;
+//     receiving_task.await?;
+
+//     Ok(())
+// }
 
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use async_compat::{Compat};
+use async_executor::LocalExecutor;
 use std::net::*;
 use crate::resolver::hyper::HyperResolver;
 mod resolver;
@@ -18,10 +19,15 @@ fn main() -> smol::io::Result<()> {
     let server = args.server.unwrap_or("https://1.1.1.1/dns-query".to_string());
 
     smol::block_on(Compat::new(async {
+        let local_ex = LocalExecutor::new();
+
+
         let binding_socket = SocketAddr::from(([0, 0, 0, 0], port));
         let resolver = HyperResolver::new(server).await;
-        let resolver = resolver.map(|x| std::rc::Rc::new(x));
-        let _success = resolver.map(|x| crate::listener::udp::start(binding_socket, x));
+        let resolver = resolver.expect("broken");
+        let resolver = std::rc::Rc::new(resolver);
+
+        let _ = crate::listener::udp::UdpServer::new(&local_ex, binding_socket, resolver).await;
        
         kill().await?;
 
