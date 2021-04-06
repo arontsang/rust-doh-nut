56127c95f3ffa412fb2fdc2d2bb88cefcf3e804a
Aron Tsang <arontsang@gmail.com>
2021-04-06 21:17:20 +0800

    Switch to static hyper client

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+#[macro_use]
+extern crate lazy_static;
 use async_compat::{Compat};
 use async_executor::LocalExecutor;
 use std::error::Error;
@@ -6,30 +8,26 @@ use crate::resolver::hyper::HyperResolver;
 use crate::listener::udp::UdpServer;
 mod resolver;
 mod listener;
-use rustop::opts;
+
+
+
 
 fn main() -> smol::io::Result<()> {
 
 
-    let (args, _) = opts! {
-        synopsis "This is a DNS stub server that proxies to a DOH server.";
-        opt port:Option<u16>, desc:"Port to host DNS proxy Defaults to: 15353";
-        opt server:Option<String>, desc:"DOH Server defaults to: https://1.1.1.1/dns-query" ;
-    }.parse_or_exit();
 
-    let port = args.port.unwrap_or(15353);
-    let server = args.server.unwrap_or("https://1.1.1.1/dns-query".to_string());
 
     smol::block_on(Compat::new(async {
         let local_ex = LocalExecutor::new();
-        let _dns_server = build_dns_server(&local_ex, server, port).await;       
+        let _google_server = build_dns_server(&local_ex, &"https://dns.google/dns-query".to_string(), 15353).await;  
+        let _cloudflare_server = build_dns_server(&local_ex, &"https://1.1.1.1/dns-query".to_string(), 15354).await;       
         local_ex.run(kill()).await
     }))
 }
 
-async fn build_dns_server(local_executor: &LocalExecutor<'_>, resolver_address: String, binding_port: u16) -> Result<UdpServer<HyperResolver>, Box<dyn Error>> {
+async fn build_dns_server(local_executor: &LocalExecutor<'_>, resolver_address: &String, binding_port: u16) -> Result<UdpServer<HyperResolver>, Box<dyn Error>> {
     let binding_socket = SocketAddr::from(([0, 0, 0, 0], binding_port));
-    let resolver = HyperResolver::new(resolver_address).await;
+    let resolver = HyperResolver::new(&resolver_address).await;
     let resolver = resolver.expect("Failed to get resolver.");
     let resolver = std::rc::Rc::new(resolver);
 
--- src/resolver/hyper.rs
+++ src/resolver/hyper.rs
@@ -3,40 +3,40 @@ use crate::resolver::DnsResolver;
 
 use async_trait::async_trait;
 use bytes::Bytes;
-use reqwest::{Body, Client, Method};
+use reqwest::{Body, Method};
 use std::error::Error;
 
-
 pub struct HyperResolver {
-    client : Client,
     server : String
 }
+
+lazy_static! {
+    static ref CLIENT: reqwest::Client = {
+        reqwest::Client::builder()
+            .tcp_nodelay(true)
+            .pool_max_idle_per_host(16)
+            .build()
+            .expect("")
+    };
+}
+
 
 
 impl HyperResolver {
 
-    pub async fn new(server: String) -> Result<HyperResolver, Box<dyn Error>> {
-
-        let client = reqwest::Client::builder()
-            .tcp_nodelay(true)
-            .pool_max_idle_per_host(16)
-            .build()?;
-
-        Result::Ok(HyperResolver { client, server })
+    pub async fn new(server: &String) -> Result<HyperResolver, Box<dyn Error>> {
+        Result::Ok(HyperResolver { server: server.clone() })
     }
-
 }
 
 #[async_trait]
 impl DnsResolver for HyperResolver {
 
     async fn resolve(&self, query: Bytes) -> Result<Bytes, Box<dyn Error>> {
-
-        let client = &self.client;
         let length = query.len().to_string();
 
         let body = Body::from(query);
-        let request = client.request(Method::POST, &self.server)
+        let request = CLIENT.request(Method::POST, &self.server)
             .header("accept", "application/dns-message")
             .header("content-type", "application/dns-message")
             .header("content-length", length)
